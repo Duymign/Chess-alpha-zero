8e89fcbb3ce9b7fad6ffc66af6f4f50fc549129f
Duymign1105 <147126083+Duymign@users.noreply.github.com>
2025-05-01 09:41:57 +0700

    test 4

--- src/chess_zero/worker/optimize.py
+++ src/chess_zero/worker/optimize.py
@@ -66,7 +66,7 @@ class OptimizeWorker:
         shuffle(self.filenames)
         total_steps = self.config.trainer.start_total_steps
 
-        while True:
+        for i in range(5):
             self.fill_queue()
             steps = self.train_epoch(self.config.trainer.epoch_to_checkpoint)
             total_steps += steps
